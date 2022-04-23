c7e2c1ccfde389f6011df903f5a2542b82860800
Mitia Ranaivo Harisoa <mitia.ranaivo-harisoa@universite-paris-saclay.fr>
2022-04-23 11:58:13 +0000

    Soumission depuis ccc53a721320 par Mitia Ranaivo Harisoa

---
+++ Ensemble.hpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int const MAXCARD  = 10;
+
+struct Ensemble{
+    //ATTRIBUTS*********************************************************
+    array<int, MAXCARD> tab;
+    int card;
+
+    //METHODES *********************************************************
+
+    //renvoie true si le tableau est vide et false sinon
+    bool estVide() const;
+
+    //Renvoie le nombre d'élement d'un tableau (différent de la taille du tableau)
+    int cardinal() const;
+
+    //ajoute un élément à l'ensemble 
+    void ajoute(int a);
+    
+    //renvoie un élement pris au hasard dans l'ensemble et le suprime
+    int tire();
+
+    
+
+    //CONSTRUCTEURS ****************************************************
+    Ensemble();
+
+};
+
+//surcharge de << pour un ensemble
+ostream& operator<<(ostream &out, Ensemble e);
+
+void swap(int &a, int &b);
